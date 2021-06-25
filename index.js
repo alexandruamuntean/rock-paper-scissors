3d3b6e8a89d222d613325ce25c08e53b17801fdd
Alex Muntean <alexandru.a.muntean@gmail.com>
2021-06-25 19:56:48 +0100

    add logic to play multiple times

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-function rps() {
-  let input = prompt("Rock/Paper/Scissors?").toLowerCase();
-  let randomize = Math.floor(Math.random() * 10);
+function rps(input, machine) {
+  if (machine <= 3) machine = "rock";
+  else if (machine > 3 && machine <= 6) machine = "paper";
+  else machine = "scissors";
 
-  if (randomize <= 3) randomize = "rock";
-  else if (randomize > 3 && randomize <= 6) randomize = "paper";
-  else randomize = "scissors";
-
-  if (input == randomize) console.log("draw");
-  else if (input == "rock" && randomize == "paper") console.log("lost");
-  else if (input == "rock" && randomize == "scissors") console.log("won");
-  else if (input == "paper" && randomize == "scissors") console.log("lost");
-  else if (input == "paper" && randomize == "rock") console.log("won");
-  else if (input == "scissors" && randomize == "rock") console.log("lost");
-  else if (input == "scissors" && randomize == "paper") console.log("won");
+  if (input == machine) console.log("draw");
+  else if (input == "rock" && machine == "paper") console.log("lost");
+  else if (input == "rock" && machine == "scissors") console.log("won");
+  else if (input == "paper" && machine == "scissors") console.log("lost");
+  else if (input == "paper" && machine == "rock") console.log("won");
+  else if (input == "scissors" && machine == "rock") console.log("lost");
+  else if (input == "scissors" && machine == "paper") console.log("won");
   console.log(input);
-  console.log(randomize);
+  console.log(machine);
 }
 
-rps();
+function play(games) {
+  let count = 0;
+  while (count < games) {
+    const input = prompt("Rock/Paper/Scissors?").toLowerCase();
+    const randomize = Math.floor(Math.random() * 10);
+    rps(input, randomize);
+    count++;
+  }
+}
+play(3);
